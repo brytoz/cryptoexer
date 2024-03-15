d12924ec4dc6a4085193586ba055455d8bcc43f1
MAC <mac@Brytoz-macBook-Pro.local>
2024-03-15 19:46:28 +0100

    modified select button

--- frontend/src/dir/pages/Dex.jsx
+++ frontend/src/dir/pages/Dex.jsx
@@ -1,8 +1,8 @@
-import { Fragment } from 'react'
-import { RiArrowLeftRightFill } from 'react-icons/ri'
-import { AiFillSetting } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
-
+import { Fragment } from "react";
+import { RiArrowLeftRightFill } from "react-icons/ri";
+import { AiFillSetting } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import locker from "../../assets/locker.png";
 export default function Dex() {
   return (
     <Fragment>
@@ -13,20 +13,30 @@ export default function Dex() {
               className="bg-white p-12 rounded-xl max-w-lg"
               style={{
                 background:
-                  'linear-gradient(180deg, #321450 0%, #1D1C1F 23.06%)',
+                  "linear-gradient(180deg, #321450 0%, #1D1C1F 23.06%)",
               }}
             >
               <div className="text-4xl text-center" id="info">
                 Ìyálọ́jà
               </div>
               <div className="text-sm pt-4">
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, non ut nostrum, blanditiis"
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
+                non ut nostrum, blanditiis"
               </div>
               <div className="mt-8">
                 <div id="offIt">
                   <span className="text-right flex justify-end text-xl pb-2 cursor-pointer">
                     <AiFillSetting />
                   </span>
+
+                  <div className="px-3 pt-4 pb-2 bg-gray-600/50  shadow-xl shadow-black text-base mb-4 flex items-center justify-between">
+                    <div>
+                      <div>Fiat</div>
+                      <div>to</div>
+                      <div>Crypto</div>
+                    </div>
+                    <div></div>
+                  </div>
                   <div className="mb-4">
                     <div className="px-3 pt-4 pb-2 bg-gray-600/50  shadow-xl shadow-black">
                       <select
@@ -60,26 +70,33 @@ export default function Dex() {
                         id="countries"
                         className="bg-transparent  text-white text-xl block w-full p-2.5 "
                       >
-                        <option disabled className="bg-black">To:</option>
+                        <option disabled className="bg-black">
+                          To:
+                        </option>
                         <option className="bg-black">Coach</option>
                         <option className="bg-black">EOS</option>
                         <option className="bg-black"> BTC</option>
                         <option className="bg-black"> ONE</option>
                       </select>
                       <div className="text-sm font-extrabold flex justify-center  text-left pl-3">
-                        <input type="text" className="flex justify-begin p-2 opacity-75 w-full bg-transparent text-left text-white "
-                          placeholder="Enter amount from 10"/>
+                        <input
+                          type="text"
+                          className="flex justify-begin p-2 opacity-75 w-full bg-transparent text-left text-white "
+                          placeholder="Enter amount from 10"
+                        />
                       </div>
                     </div>
                   </div>
-                  <div className='text-sm text-left font-normal mb-3 text-yellow-400'>
+                  <div className="text-sm text-left font-normal mb-3 text-yellow-400">
                     hello
                   </div>
 
                   <div className="mb-6 text-center">
                     <Link to="/Goal">
-                      <button className="text-xl font-extrabold bg-gradient-to-r from-red-500  to-red-900 w-full px-4 py-2 font-bold text-white  hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                        type="submit">
+                      <button
+                        className="text-xl font-extrabold bg-gradient-to-r from-red-500  to-red-900 w-full px-4 py-2 font-bold text-white  hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                        type="submit"
+                      >
                         Swap
                       </button>
                     </Link>
@@ -91,5 +108,5 @@ export default function Dex() {
         </div>
       </div>
     </Fragment>
-  )
+  );
 }
